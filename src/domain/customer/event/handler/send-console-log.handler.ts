47bc2fa2910514920c9565befd2a810814425ea8
Gabriele Almeida <naogabe@gmail.com>
2023-11-20 14:21:21 -0300

    fix: change type of EnviaConsoleLogHandler to ChangeAddressEvent

--- src/domain/customer/event/handler/send-console-log.handler.ts
+++ src/domain/customer/event/handler/send-console-log.handler.ts
@@ -2,7 +2,7 @@ import EventHandlerInterface from "../../../@shared/event/event-handler.interfac
 import CustomerCreatedEvent from "../customer-created.event";
 import ChangeAddressEvent from "../change-address.event";
 
-export default class EnviaConsoleLogHandler implements EventHandlerInterface<CustomerCreatedEvent> {
+export default class EnviaConsoleLogHandler implements EventHandlerInterface<ChangeAddressEvent> {
     handle(event: ChangeAddressEvent): void {
         console.log(`Endereço do cliente: (${event.eventData.id}, ${event.eventData.name}) alterado para: ${event.eventData.address}`);
     }
